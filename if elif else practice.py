dbee08ee2855d42459bd89fa594f3c28fcfc849a
PRao16 <48734576+PRao16@users.noreply.github.com>
2023-07-11 10:03:41 -0500

    Add files via upload

---
+++ if elif else practice.py
@@ -0,0 +1,40 @@
+# Write your code here :-) If statements: If statements are used to test for paricular conditions and srespond appropriately.
+#conditional tests.
+
+#equals   x == 42
+#not equal x !=42
+#greater than x>42
+#grether than or equal to    x>=42
+#less than or equal to       x,=42
+#less than or euqal to       x<=42
+
+#Conditional test with lists.
+bikes = ['trek']
+'trek' in bikes
+'surly' not in bikes
+#Assigning boolean values
+
+game_active = True
+can_edit = False
+int
+#if, elif, else statements
+age =int(input ('Please enter your age : '))
+if age >= 18:
+    print('you can vote. you have satisfied the  minimum age. ')
+
+elif age< 18:
+    print('you are too young to vote. you are only ' + str(age) + ' years old')
+
+    #another example
+age =int(input ('Please enter your age : '))
+
+if age < 4:
+    print('ticket_price is ' + '2')
+
+elif age < 18:
+    ticket_price = 10
+elif age > 100:
+    ticket_price + 1
+
+else:
+    ticket_price = 15
